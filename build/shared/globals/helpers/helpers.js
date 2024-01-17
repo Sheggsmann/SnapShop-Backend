27928393a96370fd6604122fbd6e8118cb238f7b
Sheggsmann <promisesheggs@gmail.com>
2024-01-17 03:08:38 -0800

    chore: merged with development branch

--- build/shared/globals/helpers/helpers.js
+++ build/shared/globals/helpers/helpers.js
@@ -51,5 +51,16 @@ class Helpers {
         const total = order.products.reduce((acc, item) => (acc += item.product.price * item.quantity), 0);
         return Helpers.calculateServiceFee(total);
     }
+    static calculateDistance(lat1, lon1, lat2, lon2) {
+        const toRadians = (degrees) => (degrees * Math.PI) / 180;
+        const dlat = toRadians(lat2 - lat1);
+        const dlon = toRadians(lon2 - lon1);
+        const a = Math.sin(dlat / 2) ** 2 +
+            Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) * Math.sin(dlon / 2) ** 2;
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        const radius = 6378.1;
+        const distance = radius * c;
+        return distance;
+    }
 }
 exports.Helpers = Helpers;
