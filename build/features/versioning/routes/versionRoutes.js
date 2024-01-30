fb284a61df3a9b37a7077e90b4cf3365f0c1dc5d
Sheggsmann <promisesheggs@gmail.com>
2024-01-30 10:03:35 -0800

    added production build

--- build/features/versioning/routes/versionRoutes.js
+++ build/features/versioning/routes/versionRoutes.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.versionRoutes = void 0;
+const auth_middleware_1 = require("../../../shared/globals/middlewares/auth-middleware");
+const user_interface_1 = require("../../user/interfaces/user.interface");
 const create_version_1 = require("../controllers/create-version");
 const get_version_1 = require("../controllers/get-version");
 const update_version_1 = require("../controllers/update-version");
@@ -15,8 +17,8 @@ class VersionRoutes {
     routes() {
         this.router.get('/app-version', get_version_1.getVersion.appVersion);
         this.router.get('/app-version/:app', get_version_1.getVersion.appVersion);
-        this.router.put('/app-version', update_version_1.updateVersion.appVersion);
-        this.router.post('/app-version', create_version_1.addVersion.appVersion);
+        this.router.put('/app-version', auth_middleware_1.authMiddleware.protect, auth_middleware_1.authMiddleware.restrictTo([user_interface_1.Role.Admin]), update_version_1.updateVersion.appVersion);
+        this.router.post('/app-version', auth_middleware_1.authMiddleware.protect, auth_middleware_1.authMiddleware.restrictTo([user_interface_1.Role.Admin]), create_version_1.addVersion.appVersion);
         return this.router;
     }
 }
